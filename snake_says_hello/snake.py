e3358aa08832d61b93596af854e98b20bc40e48c
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-12-08 21:40:14 -0600

    example

--- snake_says_hello/snake.py
+++ snake_says_hello/snake.py
@@ -16,7 +16,7 @@ GRrETING = "Hello, World!"
 # Undefined variableaa
 print(GREETING)
 
-# Function call with too many positional argumentsad
+# Function call with too many positional argumentsadd
 say_hello("Alice")
 
 
