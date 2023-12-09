e97a108bcb06f72ea4988c467b0b39ba304a643c
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-12-08 20:38:36 -0600

    example file

---
+++ snake_says_hello/snake.py
@@ -0,0 +1,29 @@
+# Example Python file with intentional Pylint issues
+
+
+def say_hello(name):
+    # Missing docstring
+    print(f"Hello, {name}")
+
+
+# Unused variable
+greeting = "Hello, World!"
+
+# Undefined variable
+print(greet)
+
+# Function call with too many positional arguments
+say_hello("Alice", "Bob")
+
+# Function call with too few positional arguments
+say_hello()
+
+
+# Unused parameter
+def unused_param_example(param1, param2):
+    print(param1)
+
+
+# Variable redefined
+number = 42
+number = "forty-two"
